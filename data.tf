aef68c1f0103bc10e2fc8e4ba1cb784b878f3bd8
YaseenBabaShaik <babashaik.y@gmail.com>
2024-12-28 10:43:33 +0530

    k

---
+++ data.tf
@@ -0,0 +1,10 @@
+data "aws_kms_key" "key" {
+  key_id = "alias/roboshop"
+}
+
+data "aws_ami" "centos8" {
+  most_recent = true
+  name_regex  = "ansible-installed"
+  owners      = ["633788536644"]
+}
+
